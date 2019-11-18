c1162a590240e68d485e45bf86f24308942abaec
SalmaFarhani-E41181641 <safarhani69@gmail.com>
2019-11-18 19:37:50 +0700

    mengubah file php

--- E41181807-DindaAyuNafisyah/projek/php-projek/daftar.php
+++ E41181807-DindaAyuNafisyah/projek/php-projek/daftar.php
@@ -1,21 +1,39 @@
-<?php
-   session_start();
-   if(isset($_SESSION['NAMA_USER'])) {
-   header('location:index.php'); }
-?>
+<!-- <?php
 
-<title>Form Pendaftaran</title>
-<div align='center'>
-  <form action="prosesdaftar.php" method="post">
-  <table>
-  <tbody>
-  <tr><td colspan="2" align="center"><h1>Daftar Baru</h1></td></tr>
-  <tr><td>Username</td><td> : <input name="username" type="text"></td></tr>
-  <tr><td>Password</td><td> : <input name="password" type="password"></td></tr>
-  <tr><td>Alamat E-mail</td><td> : <input name="e-mail" type="text"></td></tr>
-  <tr><td colspan="2" align="right"><input value="Daftar" type="submit"> <input value="Batal" type="reset"></td></tr>
-  <tr><td colspan="2" align="center">Sudah Punya akun ? <a href="login.php"><b>Login</b></a></td></tr>
-  </tbody>
-  </table>
-  </form>
-</div>
+  //  session_start();
+  //  if(isset($_SESSION['NAMA_USER'])) {
+  //  header('location:index.php'); }   
+
+   //mencari kode yang paling besar atau kode yang baru masuk
+  //  $cari_id = mysqli_query("select max(id) as kode from user");
+  //  $tm_cari= mysqli_fetch_array($cari_id);
+   //mengambil string mulai dari karakter pertama 'A' dan mengambil 4 karakter setelahnya
+  //  $kode= substr($tm_cari['kode'], 1, 4);
+   //kode yang sudah dipecah ditambah 1
+  //  $tambah= $kode+1;
+   //jika kode lebih kecil dari 10(9,8,7,6 dst)maka
+  //  if ($tambah<10){
+  //    $id="U000".$tambah;
+  //  }else{
+  //      $id="U00".$tambah;
+  //    }
+  //    if (isset($_POST['daftar'])){
+  //      mysqli_query("INSERT INTO 'simba_database'.'user'('ID_USER', 'NAMA_USER','_EMAIL_USER','PASSWORD_USER') VALUES ('$_POST [id]', '$_POST[username]', '$_POST[email]', '$_POST[pass]')"); }
+   
+//
+
+// <title>Form Pendaftaran</title>
+// <div align='center'>
+//   <form action="prosesdaftar.php" method="post">
+//   <table>
+//   <tbody>
+//   <tr><td colspan="2" align="center"><h1>Daftar Baru</h1></td></tr>
+//   <tr><td>Username</td><td> : <input name="username" type="text"></td></tr>
+//   <tr><td>Password</td><td> : <input name="password" type="password"></td></tr>
+//   <tr><td>Alamat E-mail</td><td> : <input name="e-mail" type="text"></td></tr>
+//   <tr><td colspan="2" align="right"><input value="Daftar" type="submit" name="daftar"> <input value="Batal" type="reset"></td></tr>
+//   <tr><td colspan="2" align="center">Sudah Punya akun ? <a href="login.php"><b>Login</b></a></td></tr>
+//   </tbody>
+//   </table>
+//   </form>
+// </div> -->

e4107ced976c0a81388ee2493a77ba5d301fe0a7
SalmaFarhani-E41181641 <safarhani69@gmail.com>
2019-12-30 20:58:08 +0700

    memperbaiki file hapus daftar

--- E41181641-SalmaFarhani/hapus_daftar.php
+++ E41181641-SalmaFarhani/hapus_daftar.php
@@ -6,9 +6,14 @@ if (isset($_GET['ID_DAFTAR'])) {
 
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'delete') {
-            $result = mysqli_query($koneksi, "DELETE FROM daftar WHERE 
-            ID_DAFTAR= '$ID_DAFTAR'");
+            $result =  
+            mysqli_query($koneksi, "DELETE FROM bayar WHERE ID_DAFTAR= '$ID_DAFTAR'") or die(mysqli_error($koneksi));
+            mysqli_query($koneksi, "DELETE FROM melakukan WHERE ID_DAFTAR= '$ID_DAFTAR'") or die(mysqli_error($koneksi));
+            mysqli_query($koneksi, "DELETE FROM daftar WHERE ID_DAFTAR= '$ID_DAFTAR'") or die(mysqli_error($koneksi));
         }
-    }
+        header("Location: daftar.php");
+            
+        }
+    
 }
 ?>
